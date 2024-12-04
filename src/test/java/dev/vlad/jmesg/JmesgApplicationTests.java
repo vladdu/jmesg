9958750034898836776eaae093f5c4b7e1b2f2f1
Vlad Dumitrescu <vladdu55@gmail.com>
2024-12-04 21:22:00 +0100

    demo jms spring app

---
+++ src/test/java/dev/vlad/jmesg/JmesgApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vlad.jmesg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmesgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
